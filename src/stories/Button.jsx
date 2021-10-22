577b7fee27d02d4473791163749fe34286a25c18
Salah Eddine Lalami <50052356+idurar@users.noreply.github.com>
2021-10-22 22:17:22 +0100

    Fix a bug when try to change background color

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -11,7 +11,9 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
     <button
       type="button"
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
+      style={{
+        backgroundColor: backgroundColor,
+      }}
       {...props}
     >
       {label}
