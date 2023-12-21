b114f8cebc85f7a6050245f15541710615176d66
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 21:13:56 +0000

    Deps: Updates for Storybook 7.6

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,13 +1,13 @@
-import { within, userEvent } from '@storybook/testing-library';
+import { within, userEvent, expect } from "@storybook/test";
 
-import { Page } from './Page';
+import { Page } from "./Page";
 
 export default {
-  title: 'Example/Page',
+  title: "Example/Page",
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 };
 
@@ -17,9 +17,12 @@ export const LoggedOut = {};
 export const LoggedIn = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
-      name: /Log in/i,
-    });
+    const loginButton = canvas.getByRole("button", { name: /Log in/i });
+    await expect(loginButton).toBeInTheDocument();
     await userEvent.click(loginButton);
+    await expect(loginButton).not.toBeInTheDocument();
+
+    const logoutButton = canvas.getByRole("button", { name: /Log out/i });
+    await expect(logoutButton).toBeInTheDocument();
   },
 };
