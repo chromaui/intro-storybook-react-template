6922becc64e2578c52134865164001a83482a040
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-09-09 21:14:08 +0100

    Removes addon links from the project

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-docs",
     "@storybook/addon-vitest",
     "@chromatic-com/storybook"
